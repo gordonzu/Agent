913bc09b389ae2db884245dba3b0098ccd52762b
gordonzu <gzulux@gmail.com>
2017-04-03 15:47:42 -0400

    Implement Dynamic Percept Test

--- test/agent/AgentTestSuite.java
+++ test/agent/AgentTestSuite.java
@@ -3,13 +3,14 @@ package agent;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//import aima.test.core.unit.agent.impl.DynamicPerceptTest;
+import agent.impl.DynamicPerceptTest;
 //import aima.test.core.unit.agent.impl.PerceptSequenceTest;
 import agent.impl.aprog.TableDrivenAgentProgramTest;
 import agent.impl.aprog.simplerule.RuleTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ RuleTest.class, TableDrivenAgentProgramTest.class })
+@Suite.SuiteClasses({ RuleTest.class, TableDrivenAgentProgramTest.class,
+                    DynamicPerceptTest.class })
 public class AgentTestSuite {
 
 }
